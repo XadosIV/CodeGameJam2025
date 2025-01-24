ee66f82b6c2f2146964506e730d4437c44bd6900
SoraxDubbing <sorax5.r23@gmail.com>
2025-01-24 18:00:41 +0100

    add gray screen

--- Scripts/MentalHealthEffect.gd
+++ Scripts/MentalHealthEffect.gd
@@ -1,7 +1,8 @@
 extends Node
 
 @onready var rect = $ColorRect
-@export var shader : ShaderMaterial
+@export var shader: ShaderMaterial
+@export var env: Environment
 
 @export var max_chaos: int = 32
 @export var effect_curve: Curve
@@ -11,6 +12,7 @@ const value: String = "chaos"
 func _ready() -> void:
 	GameManager.mental_health_changed.connect(_on_health_changed)
 	shader.set_shader_parameter(value, 0)
+	env.adjustment_saturation = 1
 	
 func _on_health_changed(newValue):
 	var percentage: float = newValue / GameManager.max_mental
@@ -18,3 +20,4 @@ func _on_health_changed(newValue):
 	var effect: float = effect_curve.sample(percentage)
 	var chaos: int = int(effect * max_chaos)
 	shader.set_shader_parameter(value,chaos)
+	env.adjustment_saturation = 1 - effect
