433d8a8b18a51213d089965e616dd78d36fef047
SoraxDubbing <sorax5.r23@gmail.com>
2025-01-24 19:31:39 +0100

    add basic menu component with some options

--- Scripts/audio_controller.gd
+++ Scripts/audio_controller.gd
@@ -1,50 +1,91 @@
 extends Node2D
 
-var sounds: Array[String]
-var sounds_loaded: Array[AudioStream]
-var musics: Array[String]
-var musics_loaded: Array[AudioStream]
+var sounds: Array[String] = []
+var sounds_loaded: Array[AudioStream] = []
+var musics: Array[String] = []
+var musics_loaded: Array[AudioStream] = []
 
-func _ready() -> void:		
+var global_volume: float = 1.0  # Volume global, 1.0 = 100%, 0.5 = 50%, etc.
+
+func _ready() -> void:
 	search_all_files("res://Audio")
 	print("Musics: ", musics)
 	print("Sounds: ", sounds)
 	
 	load_all_audios()
 	
-	music_mixer(15)
+	set_music_volume_percentage(1.0)
 	play_music(6)
-	sound_mixer(-15)
+	set_sound_volume_percentage(1.0)
 	play_sound(0)
 
-func play_music(index:int):
+func play_music(index: int):
 	if index >= 0 and index < musics_loaded.size():
 		$Music.stream = musics_loaded[index]
 		$Music.play()
-	
-func play_sound(index:int):
+
+func play_sound(index: int):
 	if index >= 0 and index < sounds_loaded.size():
 		$Sound.stream = sounds_loaded[index]
 		$Sound.play()
 		
-func music_mixer(volume:int):
-	$Music.set_volume_db(volume)
-	
-func sound_mixer(volume:int):
-	$Sound.set_volume_db(volume)
-	
+func set_music_volume(volume: int):
+	$Music.set_volume_db(volume * global_volume)
+
+func set_sound_volume(volume: int):
+	$Sound.set_volume_db(volume * global_volume)
+
+func set_music_volume_percentage(percentage: float):
+	# Définit le volume de la musique en pourcentage (0.0 - 1.0)
+	percentage = clamp(percentage, 0.0, 1.0)
+	var db = percentage_to_db(percentage)
+	set_music_volume(db)
+
+func set_sound_volume_percentage(percentage: float):
+	# Définit le volume du son en pourcentage (0.0 - 1.0)
+	percentage = clamp(percentage, 0.0, 1.0)
+	var db = percentage_to_db(percentage)
+	set_sound_volume(db)
+
+func get_music_volume() -> float:
+	# Retourne le volume de la musique en pourcentage (0.0 - 1.0)
+	var db = $Music.get_volume_db()
+	return db_to_percentage(db)
+
+func get_sound_volume() -> float:
+	# Retourne le volume du son en pourcentage (0.0 - 1.0)
+	var db = $Sound.get_volume_db()
+	return db_to_percentage(db)
+
+func db_to_percentage(db: float) -> float:
+	# Convertit un volume en décibels (dB) en une valeur entre 0.0 et 1.0
+	if db <= -80.0:
+		return 0.0
+	return pow(10, db / 20)
+
+func percentage_to_db(percentage: float) -> float:
+	# Convertit une valeur entre 0.0 et 1.0 en décibels (dB)
+	if percentage <= 0.0:
+		return -80.0  # Volume minimal
+	return 20 * (log(percentage) / log(10))
+
 func pause_sound():
 	$Sound.set_stream_paused(!$Sound.get_stream_paused())
-	
+
 func pause_music():
 	$Music.set_stream_paused(!$Music.get_stream_paused())
 
 func play_music2D():
 	$Music2D.play()
-	
+
 func play_sound2D():
 	$Sound2D.play()
-	
+
+func set_global_volume(value: float):
+	global_volume = clamp(value, 0.0, 1.0)  # Le volume global doit être entre 0 et 1
+	set_music_volume($Music.get_volume_db())
+	set_sound_volume($Sound.get_volume_db())
+
 func search_all_files(base_path: String):
 	var dir = DirAccess.open(base_path)
 	if !dir:
--- ui/options_component.gd
+++ ui/options_component.gd
@@ -1,14 +1,47 @@
 extends Control
 
-@onready var volume_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/VolumeAmount
+@onready var global_label = $MarginContainer/VBoxContainer/Volumes/Global/Label
+@onready var music_label = $MarginContainer/VBoxContainer/Volumes/Music/Label
+@onready var sfx_label = $MarginContainer/VBoxContainer/Volumes/Sound/Label
+
+@onready var global_slider = $MarginContainer/VBoxContainer/Volumes/Global/Volume
+@onready var music_slider = $MarginContainer/VBoxContainer/Volumes/Music/Volume
+@onready var sfx_slider = $MarginContainer/VBoxContainer/Volumes/Sound/Volume
 
 @export var main_menu: String = "res://Scenes/ui/MainMenu.tscn"
 
 func _ready() -> void:
-	volume_label.text = str(0)
-
-func _on_volume_value_changed(value: float) -> void:
-	volume_label.text = str(value)
+	global_label.text = str(AudioController.global_volume)
+	music_label.text = str(AudioController.get_music_volume())
+	sfx_label.text = str(AudioController.get_sound_volume())
+	
+	global_slider.value = AudioController.global_volume
+	music_slider.value = AudioController.get_music_volume()
+	sfx_slider.value = AudioController.get_sound_volume()
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file(main_menu)
+
+
+func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
+	var displayType: int = 0
+	if toggled_on:
+		displayType = DisplayServer.WINDOW_MODE_FULLSCREEN
+	else:
+		displayType = DisplayServer.WINDOW_MODE_WINDOWED
+	DisplayServer.window_set_mode(displayType)
+
+
+func _on_global_value_changed(value: float) -> void:
+	AudioController.global_volume = value
+	global_label.text = str(value)
+
+
+func _on_music_value_changed(value: float) -> void:
+	AudioController.set_music_volume_percentage(value)
+	music_label.text = str(value)
+
+
+func _on_sound_value_changed(value: float) -> void:
+	AudioController.set_sound_volume_percentage(value)
+	sfx_label.text = str(value)
