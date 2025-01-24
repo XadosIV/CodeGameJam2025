f4c3db588ca8568271d0e7a4b5c5c9ce1f242669
XadosIV <83866570+XadosIV@users.noreply.github.com>
2025-01-24 20:09:27 +0100

    PAS DE CONFLIT LETZGOO

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -10,7 +10,8 @@ var mental_health: float = max_mental
 
 @export var music_key: String = "ui_music"
 
-var player_pos = Vector2(200,0)
+var enter_side = ""
+var corridor_offset = 0
 var current_animation = "face"
 var last_dir = Vector2.ZERO
 
@@ -37,35 +38,25 @@ func _input(event: InputEvent) -> void:
 		is_playing = false
 		
 
-func change_scene():
+func change_scene(offset, side, player):
 	var name = get_tree().current_scene.scene_file_path
 	var mapcord = name.split("/")[4].split(".")[0].split("_")
-	
-	var player = get_tree().current_scene.get_node("Melody")
-	var ppos = player.position
-	
 	current_animation = player.current_animation
 	last_dir = player.last_direction
-	
-	var border = get_tree().current_scene.get_node("MapBorder").get_node("Shape").shape.get_rect()
-	
-	if ppos.x < border.position.x: #gauche -> droite
+	var ppos = player.position
+	if side == "left": #gauche -> droite
 		mapcord[0] = str(int(mapcord[0])-1)
-		player_pos[0] = border.end.x -1
-		player_pos[1] = ppos[1]
-	elif ppos.x > border.end.x: #droite -> gauche
+		enter_side = "right"
+	elif side == "right": #droite -> gauche
 		mapcord[0] = str(int(mapcord[0])+1)
-		player_pos[0] = border.position.x +1
-		player_pos[1] = ppos[1]
-	elif ppos.y < border.position.y: #haut -> bas
+		enter_side = "left"
+	elif side == "top": #haut -> bas
+		enter_side = "bot"
 		mapcord[1] = str(int(mapcord[1])-1)
-		player_pos[1] = border.end.y -1
-		player_pos[0] = ppos[0]
-	elif ppos.y > border.end.y: #bas -> haut
+	elif side == "bot": #bas -> haut
 		mapcord[1] = str(int(mapcord[1])+1)
-		player_pos[1] = border.position.y +1
-		player_pos[0] = ppos[0]
+		enter_side = "top"
 	
-	
+	corridor_offset = offset
 	print("changed to : " + mapcord[0]+"_"+mapcord[1]+".tscn")
 	get_tree().change_scene_to_file("res://Scenes/Map/"+mapcord[0]+"_"+mapcord[1]+".tscn")
--- Scripts/warper.gd
+++ Scripts/warper.gd
@@ -1,16 +1,22 @@
 extends Area2D
 
-func _ready():
+var left = []
+var right = []
+var top = []
+var bot = []
+var border
+var initiated = false
+
+func _enter_tree():
+	var magic_number = get_parent().get_node("%Melody").get_node("CollisionShape2D").position.y * 2
 	var map = get_parent().get_node("TileMapLayer")
-	var border = get_node("Shape").shape.get_rect()
+	border = get_node("Shape").shape.get_rect()
 	
 	#find top exit
 	var y = border.position.y
-	var top = []
 	for i in range(border.position.x, border.end.x, 32):
 		var mapcoor = map.local_to_map(Vector2(i, y))
 		if map.get_cell_source_id(mapcoor) != -1:
-			map.erase_cell(mapcoor)
 			top.append(map.map_to_local(mapcoor)[0]-16)
 			mapcoor[0] += 2
 			top.append(map.map_to_local(mapcoor)[0]-16)
@@ -18,11 +24,9 @@ func _ready():
 	
 	#find bot exit
 	y = border.end.y
-	var bot = []
 	for i in range(border.position.x, border.end.x, 32):
 		var mapcoor = map.local_to_map(Vector2(i, y))
 		if map.get_cell_source_id(mapcoor) != -1:
-			map.erase_cell(mapcoor)
 			bot.append(map.map_to_local(mapcoor)[0]-16)
 			mapcoor[0] += 2
 			bot.append(map.map_to_local(mapcoor)[0]-16)
@@ -30,29 +34,43 @@ func _ready():
 			
 	#find left exit
 	var x = border.position.x
-	var left = []
 	for i in range(border.position.y, border.end.y, 32):
 		var mapcoor = map.local_to_map(Vector2(x, i))
 		if map.get_cell_source_id(mapcoor) != -1:
-			map.erase_cell(mapcoor)
-			left.append(map.map_to_local(mapcoor)[1]-16)
+			left.append(map.map_to_local(mapcoor)[1]-16)# - magic_number)
 			mapcoor[1] += 2
-			left.append(map.map_to_local(mapcoor)[1]-16)
+			left.append(map.map_to_local(mapcoor)[1]-16)# - magic_number)
 			break
 
 	#find right exit
 	x = border.end.x
-	var right = []
 	for i in range(border.position.y, border.end.y, 32):
 		var mapcoor = map.local_to_map(Vector2(x, i))
 		if map.get_cell_source_id(mapcoor) != -1:
-			map.erase_cell(mapcoor)
-			right.append(map.map_to_local(mapcoor)[1])
+			right.append(map.map_to_local(mapcoor)[1]-16)#- magic_number)
 			mapcoor[1] += 2
-			right.append(map.map_to_local(mapcoor)[1])
+			right.append(map.map_to_local(mapcoor)[1]-16)#- magic_number)
 			break
-
-	print(right) # [-480, -416]
+	initiated = true
 
 func _on_body_exited(body):
-	GameManager.change_scene()
+	var player = get_parent().get_node("%Melody")
+	var magic_number = player.get_node("CollisionShape2D").position.y * 2.5
+	var ppos = player.position
+	
+	var collision_side
+	var offset
+	
+	if ppos.x < border.position.x: #gauche -> droite
+		offset = ppos.y - left[0]
+		collision_side = "left"
+	elif ppos.x > border.end.x: #droite -> gauche
+		offset = ppos.y - right[0]
+		collision_side = "right"
+	elif ppos.y + magic_number < border.position.y: #haut -> bas
+		offset = ppos.x - top[0]
+		collision_side = "top"
+	elif ppos.y + magic_number > border.end.y: #bas -> haut
+		offset = ppos.x - bot[0]
+		collision_side = "bot"
+	GameManager.change_scene(offset, collision_side, player)
